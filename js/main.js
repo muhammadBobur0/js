c51d1e729e4a4e453fd036494f8ebdd96c468d60
muhammadBobur0 <muhammadboburbek@gmail.com>
2022-06-14 19:00:10 +0500

    add news

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ var elman = document.querySelector(".input-men");
 var text = document.querySelector(".man-strong");
 var eltext =document.querySelector(".text");
 var elveloso = document.querySelector(".velo-strong");
-var elinputValu = document.querySelector(".input-velo");
+var elCar = document.querySelector(".strong-car");
+var elsam = document.querySelector(".strong-sam");
+
 
 
 
@@ -21,16 +23,36 @@ elForm.addEventListener('submit', function (e){
     return
   }
 
-  eltime(elman, elvLU)
-  
+  text.textContent = human(elvLU).toFixed(2)
+  elveloso.textContent = velo(elvLU).toFixed(2)
+  elCar.textContent = car(elvLU).toFixed(2)
+  elsam.textContent = sam (elvLU).toFixed(2)
 });
 
 
-
-function eltime (elman, elvLU) {
-  return  elman == "man" ? text.textContent = Math.round (elvLU / 3.6)  : console.log(" "); 
+function human (elvLU) {
+  return elvLU / 3.6
 };
 
-function elvelo (elman, elvLU) {
-  return  elman == "car" ? elveloso.textContent = Math.round (elvLU / 3.6)  : console.log(" ") 
+function velo (elvLU) {
+  return elvLU / 20.1
 };
+
+function car (elvLU) {
+  return elvLU / 70
+};
+
+function sam (elvLU) { 
+  return elvLU / 800
+};
+ 
+ 
+
+
+// function eltime (elman, elvLU) {
+//   return  elman == "man" ? text.textContent = Math.round (elvLU / 3.6)  : console.log(" "); 
+// };
+
+// function elvelo (elman, elvLU) {
+//   return  elman == "car" ? elveloso.textContent = Math.round (elvLU / 3.6)  : console.log(" ") 
+// };
